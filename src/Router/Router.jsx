891c16e764d8a2c1eab0c51fe2d7df0049dc935e
Moinuddin-dotcom <moinchy7@gmail.com>
2025-02-22 20:02:41 +0600

    error page added

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import MainCompo from "../MotherComponent/MainCompo";
 import Home from "../ChildOfMotherCompo/Home";
 import ActivityLog from "../ChildOfMotherCompo/SectionsOfHome/ActivityLog";
+import ErrorPage from "../Components/ErrorPage/ErrorPage";
 
 export const Router = createBrowserRouter([
     {
         path: "/",
         element: <MainCompo />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
